3dc685c1bcc87100533e9f809bf4c1fe0bf6b65e
Pavel <pvvolo2200@gmail.com>
2022-03-09 15:04:41 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Foo(object):
+    __slots__ = ["a","b"]
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+    def foo(self,value):
+        return value
+
+def wrapper(self, *args,**kwargs):
+    return
+
+def app()
+
+if __name__=="__main__":
+
